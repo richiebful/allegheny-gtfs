f6d94902dffd65816842aa99401622565776b3ac
Richard Fulop <richardbfulop@gmail.com>
2019-02-10 22:20:47 -0500

    Add new data sources/begin extracting data

---
+++ osm/dataload.py
@@ -0,0 +1,29 @@
+from lxml import etree
+
+with open('export.xml') as overpass_fp:
+    overpass_tree = etree.parse(overpass_fp)
+
+routes = overpass_tree.xpath("//relation[tag/@k = 'route_master']")
+trips = []
+for rte in routes:
+    child_trip_nodes = rte.xpath("member")
+    members = []
+    for child in child_trip_nodes:
+        members.append({
+            "ref": child.xpath("@ref")[0],
+            "role": child.xpath("@role")[0]
+        })
+        trips += members
+
+    rte_spec = {
+        "id": rte.xpath("@id")[0],
+        "name": rte.xpath("tag[@k='name']/@v")[0],
+        "operator": rte.xpath("tag[@k='operator']/@v")[0],
+        "ref": rte.xpath("tag[@k='ref']/@v")[0],
+        "members": members
+    }
+
+refined_trips = []
+for trip in trips:
+    trip_node = overpass_tree.xpath("//relation[@id=$trip_id]", trip_id=trip["ref"])[0]
+    print(trip_node)
